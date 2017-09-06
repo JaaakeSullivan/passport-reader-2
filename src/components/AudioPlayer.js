a6fdea9a3b70e63494a079e422f0a1c736686eba
Jake Sullivan <jsullivan@emcp.com>
2017-09-06 14:11:17 -0500

    added closeAudio

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import ReactPlayer from 'react-player'
+import Button from 'material-ui/Button';
+import AddIcon from 'material-ui-icons/Add';
 
 
 const styles = theme => ({
@@ -16,10 +18,14 @@ function AudioPlayer(props){
 
   const classes = props.classes;
 
+  const {closeAudio} = props;
 
   return (
 
     <div className={classes.root}>
+      <Button onClick={closeAudio } fab color="primary" aria-label="add" className={classes.button}>
+          <AddIcon />
+      </Button>
       <ReactPlayer url={props.audio[0]} progressFrequency={1000} controls playbackRate={props.speed} height={50} width={'100%'} />
     </div>
   )
--- src/containers/AudioContainer.js
+++ src/containers/AudioContainer.js
@@ -23,7 +23,6 @@ const styles = theme => ({
   
 });
 
-
 const darkStyles = theme => ({
   root: {
     backgroundColor: 'red',  
